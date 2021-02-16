0ef38b2a26affffb4dbdb793b73b0954c8994bcc
Garrett <gman9mm@live.com>
2021-02-16 15:36:26 -0800

    Successful  2nd test Co-authored-by: Shannon Grantski <grantski@pm.me> Co-authored-by: Agata Kolodziej <agatakolohe@gmail.com>

--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -1,3 +1,37 @@
-export default (state = {}, action) => {
-  return state;
-};
+// export default (state = {}, action) => {
+//   const { xIsNext, squares, history } = action;
+//   switch (action.type) {
+//     case 'SAVE_TURN':
+//       return Object.assign({}, state, {
+//         [square]: {
+//           xIsNext: false,
+//           history: history
+//         }
+//       });
+//     default:
+//       return state;
+//   };
+
+
+//reducer is doing what setState is doing in our component
+
+
+// history: history.concat([{
+//   squares: squares,
+// }]),
+// xIsNext: false,
+
+export default (state = false, action) => {
+  // const { xIsNext, squares, history } = action;
+  switch (action.type) {
+    case 'SAVE_TURN':
+      return !state;
+    // ...state,
+    // history: history.concat([{
+    //   squares: squares,
+    // }]),
+    // xIsNext: false,
+    default:
+      return state;
+  };
+}
